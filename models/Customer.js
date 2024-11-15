7058219656cd2fb032839caba5886b017b43c7a4
offline171 <146153141+offline171@users.noreply.github.com>
2024-11-15 00:44:22 -0500

    Update and rename customer to Customer.js

---
+++ models/Customer.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Customer = sequelize.define('customer', {
+  customer_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING
+  },
+  state: {
+    type: DataTypes.STRING
+  },
+  city: {
+    type: DataTypes.STRING
+  },
+  street_address: {
+    type: DataTypes.STRING
+  },
+  zip_code: {
+    type: DataTypes.INTEGER
+  }
+});
+
+module.exports = Customer;
